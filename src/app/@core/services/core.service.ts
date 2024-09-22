74a1c1cac6be0720887df75aee44b1d130f86d73
Robert I <robertivascu24@gmail.com>
2024-09-22 19:46:34 +0300

    Fix environments

--- src/app/@core/services/core.service.ts
+++ src/app/@core/services/core.service.ts
@@ -1,56 +1,64 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { NbToastrService } from '@nebular/theme';
 import { BehaviorSubject, of } from 'rxjs';
 import { Observable } from 'rxjs-compat';
 import { catchError, tap } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
 import { Customer } from '../data/customer';
 import { Doctor } from '../data/doctor';
 import { Entity } from '../data/entity';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoreService {
+  private apiUrl = `${environment.apiUrl}/`;
+
   private http = inject(HttpClient);
 
   private toastrService = inject(NbToastrService);
 
-  private doctorsSource$ = new BehaviorSubject<Doctor[]>([])
+  private doctorsSource$ = new BehaviorSubject<Doctor[]>([]);
 
   readonly doctors$ = this.doctorsSource$.asObservable();
 
   getCustomers() {
-    return this.http.get<Customer[]>(`/api/${Entity.Customer}`).pipe(
-      catchError((err) => this.handleError(err))
-    );
+    return this.http
+      .get<Customer[]>(`${this.apiUrl}${Entity.Customer}`)
+      .pipe(catchError((err) => this.handleError(err)));
   }
 
   getDoctors(): Observable<Doctor[]> {
-    return this.http.get<Doctor[]>(`/api/${Entity.Doctor}`).pipe(
+    return this.http.get<Doctor[]>(`${this.apiUrl}${Entity.Doctor}`).pipe(
       catchError((err) => this.handleError(err)),
-      tap((doctors) => this.updateDoctors(doctors)))
+      tap((doctors) => this.updateDoctors(doctors))
+    );
   }
 
   postDoctors(doctor: Doctor): Observable<unknown> {
-    return this.http.post<unknown>(`/api/${Entity.Doctor}`, doctor).pipe(
-      tap(() => this.showSuccessMessage()),
-      catchError((err) => this.handleError(err)),
-    )
+    return this.http
+      .post<unknown>(`${this.apiUrl}${Entity.Doctor}`, doctor)
+      .pipe(
+        tap(() => this.showSuccessMessage()),
+        catchError((err) => this.handleError(err))
+      );
   }
 
   putDoctors(doctor: Doctor): Observable<unknown> {
-    return this.http.put<unknown>(`/api/${Entity.Doctor}`, doctor).pipe(
-      tap(() => this.showSuccessMessage()),
-      catchError((err) => this.handleError(err))
-    )
+    return this.http
+      .put<unknown>(`${this.apiUrl}${Entity.Doctor}`, doctor)
+      .pipe(
+        tap(() => this.showSuccessMessage()),
+        catchError((err) => this.handleError(err))
+      );
   }
 
   delete(entity: Entity, id: number): Observable<unknown> {
-    return this.http.delete<unknown>(`/api/${entity}/${id}`).pipe(
+    return this.http.delete<unknown>(`${this.apiUrl}${entity}/${id}`).pipe(
       tap(() => this.showSuccessMessage()),
       catchError((err) => this.handleError(err))
-    )
+    );
   }
 
   updateDoctors(doctors: Doctor[]) {
@@ -65,7 +73,9 @@ export class CoreService {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
       console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
     }
     this.toastrService.danger('Something went wrong', 'Server Error');
     // Return an observable with a user-facing error message.
@@ -73,6 +83,6 @@ export class CoreService {
   }
 
   private showSuccessMessage(): void {
-    this.toastrService.success('Operation completed', 'Success')
+    this.toastrService.success('Operation completed', 'Success');
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
 export const environment = {
   production: true,
+  apiUrl: 'https://auditive.azurewebsites.net',
 };
