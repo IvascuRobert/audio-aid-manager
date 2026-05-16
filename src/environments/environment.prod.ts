1157439e883f73ae385bf5b3603724d549e47558
Robert I <robertivascu24@gmail.com>
2026-05-16 15:05:57 +0300

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://auditive-app.azurewebsites.net',
+  apiUrl: 'https://auditiveapi.runasp.net',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://auditive-app.azurewebsites.net',
+  apiUrl: 'https://auditiveapi.runasp.net',
 };
